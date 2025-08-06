42c73a96bb37a412e3d59775240a252108c1c716
printf-hoven <61111678+printf-hoven@users.noreply.github.com>
2025-08-06 16:53:59 +0530

    Update ModelBinding.cs

--- Filters/ModelBinding.cs
+++ Filters/ModelBinding.cs
@@ -6,6 +6,9 @@ namespace Journal.Filters;
 // the following taken from
 // https://github.com/dotnet/AspNetCore.Docs/tree/2f0088ce754b123b924b7a23fced06e6fbb45fa9/aspnetcore/mvc/models/file-uploads/samples/3.x/SampleApp
 
+// filter that disables form-model binding because we shall be writing
+// our own code to read the multipart sections. 
+
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public class DisableFormValueModelBindingAttribute : Attribute, IResourceFilter
 {
@@ -20,4 +23,5 @@ public class DisableFormValueModelBindingAttribute : Attribute, IResourceFilter
   public void OnResourceExecuted(ResourceExecutedContext context)
   {
   }
+
 }
